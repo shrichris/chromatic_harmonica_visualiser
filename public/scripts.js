d12f5bdd0840437c9b51084471e9c35b63784f2e
Sriram Srinivasan <shrichris@gmail.com>
2023-03-28 19:26:47 +0100

    changes to deploy as single page app to Firebase

--- public/scripts.js
+++ public/scripts.js
@@ -7,7 +7,7 @@ document.getElementById("scale").addEventListener("change", function (event) {
   
     holes.forEach((hole) => {
       const notes = hole.dataset.notes.split(",");
-      hole.classList.remove("highlighted");
+       hole.classList.remove("highlighted");
   
       notes.forEach((note, index) => {
         const noteElement = hole.querySelector(`.note:nth-child(${index + 1})`);
