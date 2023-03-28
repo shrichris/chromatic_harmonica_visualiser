749119004b82cc724d1ea7ad94b368c84a007d97
Sriram Srinivasan <shrichris@gmail.com>
2023-03-28 14:19:17 +0100

    Initialization. "Hello World"

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/my-web-app', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
