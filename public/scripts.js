d9536de2a274e77709b49e5858885e42354f2ac1
Sriram Srinivasan <shrichris@gmail.com>
2023-03-28 15:01:04 +0100

    scale selector, initial vis. needs correction

--- public/scripts.js
+++ public/scripts.js
@@ -1,14 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const holes = document.querySelectorAll('.hole');
+  document.getElementById("scale").addEventListener("change", function (event) {
+    const scaleNotes = event.target.value.split(",");
+    const holes = document.querySelectorAll(".hole");
   
     holes.forEach((hole) => {
-      hole.addEventListener('click', (event) => {
-        const notes = event.currentTarget.dataset.notes.split(',');
-        const currentNote = event.target;
+      const notes = hole.dataset.notes.split(",");
+      hole.classList.remove("highlighted");
   
-        if (currentNote.classList.contains('note')) {
-          const noteIndex = Array.from(currentNote.parentNode.children).indexOf(currentNote);
-          alert(`You played the note: ${notes[noteIndex]}`);
+      notes.forEach((note, index) => {
+        const noteElement = hole.querySelector(`.note:nth-child(${index + 1})`);
+        noteElement.classList.remove("highlighted");
+  
+        if (scaleNotes.includes(note)) {
+          noteElement.classList.add("highlighted");
         }
       });
     });
